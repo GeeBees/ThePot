aa4e19b10c76b0b27ef8f8c75f9002f209e0763e
Fernando <fgmoralesc@gmail.com>
2015-07-18 19:41:36 -0300

    Beautify js

--- js/pot.js
+++ js/pot.js
@@ -1,53 +1,52 @@
-var millisec  =  0;
-var seconds  =  0;
+var millisec = 0;
+var seconds = 0;
 var timer;
 
-function display(){
+function display() {
 
-  if (millisec >= 9){
-    millisec = 0
-    seconds += 1
-  }
-  else
-    millisec += 1
-	$('#time').text(seconds + '.' + millisec);
+    if (millisec >= 9) {
+        millisec = 0;
+        seconds += 1;
+    } else
+        millisec += 1;
+    $('#time').text(seconds + '.' + millisec);
     //fix fullstack error, use literals with care...
     timer = setTimeout('display()', 100);
-  }
+}
 
 function startTimer() {
-	
-  if (timer > 0) {
-    return;
-  }
-  display();	
+
+    if (timer > 0) {
+        return;
+    }
+    display();
 }
+
 function stopTimer() {
-  clearTimeout(timer);
-  timer  =  0;
+    clearTimeout(timer);
+    timer = 0;
 }
 
 function startStopTimer() {
-  if (timer > 0) {
-    clearTimeout(timer);
-    timer = 0;
-  } else {
-    display();
-  }
+    if (timer > 0) {
+        clearTimeout(timer);
+        timer = 0;
+    } else {
+        display();
+    }
 }
 
 function resetTimer() {
-  millisec = 0;
-  seconds = 0;
-  $('#time').text(seconds + '.' + millisec);
+    millisec = 0;
+    seconds = 0;
+    $('#time').text(seconds + '.' + millisec);
 }
 
-$( document ).keypress(function( event ) {
-	console.log(event.keyCode);
-	if (event.keyCode == 115) {
-		startStopTimer();
-	}
-	else if(event.keyCode == 114) {
-		resetTimer();
-	}
+$(document).keypress(function(event) {
+    console.log(event.keyCode);
+    if (event.keyCode == 115) {
+        startStopTimer();
+    } else if (event.keyCode == 114) {
+        resetTimer();
+    }
 });
