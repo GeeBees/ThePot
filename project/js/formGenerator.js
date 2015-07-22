c2fdfba697f681ea36522d631e5c93757998b6f4
Fernando <fgmoralesc@gmail.com>
2015-07-22 05:12:12 -0300

    Clean code. Add form generator Some refactoring is needed though

---
+++ project/js/formGenerator.js
@@ -0,0 +1,33 @@
+// Generates an n-word formulary
+// TODO: duplicate code: refactor this
+function generateWordForm(n) {
+    var formHolder = $('<div>')
+        .addClass('form-group');
+    for (var i = 1; i <= n; i++) {
+        var tmpl = $('<input>')
+            .addClass('form-control')
+            .attr('placeholder', '')
+            .attr('id', 'group1word' + i);
+        formHolder
+            .append($('<label>').html('Word ' + i + ':'))
+            .append(tmpl)
+            .append($('<br>'));
+    }
+    return formHolder;
+}
+
+function generateNameForm(n) {
+  var formHolder = $('<div>')
+      .addClass('form-group');
+  for (var i = 1; i <= n; i++) {
+      var tmpl = $('<input>')
+          .addClass('form-control')
+          .attr('type', 'text')
+          .attr('id', 'group1Member' + i + 'Name' );
+      formHolder
+          .append($('<label>').html('Member ' + i + ' name:'))
+          .append(tmpl)
+          .append($('<br>'));
+  }
+  return formHolder;
+}
