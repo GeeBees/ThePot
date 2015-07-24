6da12b35bbe2a3726d2f30d50270b5ccb8d07a80
Fernando <fgmoralesc@gmail.com>
2015-07-23 23:14:00 -0300

    Fix duplicate code in form generator

--- project/js/formGenerator.js
+++ project/js/formGenerator.js
@@ -1,34 +1,51 @@
-// Generates an n-word formulary
-// TODO: duplicate code: refactor this
-function generateWordForm(n) {
-    var formHolder = $('<div>')
+var formHolder = function() {
+    return $('<div>')
         .addClass('form-group');
+}
+
+function generateForm(n, tmpl) {
+    var holder = formHolder();
     for (var i = 1; i <= n; i++) {
-        var tmpl = $('<input>')
-            .addClass('form-control')
-            .attr('placeholder', '')
-            .attr('id', 'group1word' + i);
-        formHolder
-            .append($('<label>').html('Word ' + i + ':'))
-            .append(tmpl)
-            .append($('<br>'));
+        var template = tmpl(i)();
+        holder
+            .append(template.label)
+            .append(template.input)
+            .append(template.br);
     }
-    return formHolder;
+    return holder;
 }
 
 function generateNameForm(n) {
-  var formHolder = $('<div>')
-      .addClass('form-group');
-  for (var i = 1; i <= n; i++) {
-      var tmpl = $('<input>')
-          .addClass('form-control')
-          .attr('type', 'text')
-          .attr('id', 'group1Member' + i + 'Name' )
-          .attr('value', 'Member ' + i);
-      formHolder
-          .append($('<label>').html('Member ' + i + ' name:'))
-          .append(tmpl)
-          .append($('<br>'));
-  }
-  return formHolder;
+    var tmpl = function(i) {
+        return function() {
+            return {
+                'label': $('<label>')
+                    .html('Member ' + i + ' name:'),
+                'input': $('<input>')
+                    .addClass('form-control')
+                    .attr('type', 'text')
+                    .attr('id', 'group1Member' + i + 'Name')
+                    .attr('value', 'Member ' + i),
+                'br': $('<br>')
+            };
+        };
+    };
+    return generateForm(n, tmpl);
+}
+
+function generateWordForm(n) {
+    var tmpl = function(i) {
+        return function() {
+            return {
+                'label': $('<label>')
+                    .html('Word ' + i + ':'),
+                'input': $('<input>')
+                    .addClass('form-control')
+                    .attr('placeholder', '')
+                    .attr('id', 'group1word' + i),
+                'br': $('<br>')
+            };
+        };
+    };
+    return generateForm(n, tmpl);
 }
