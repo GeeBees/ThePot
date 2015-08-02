54efaa7553b6b4e3372c11a45888f7dba27b5740
unknown <mario.a.vergara.l@gmail.com>
2015-08-02 00:28:35 -0300

    function shuffle(array) added: return array with the order of its elements randomized

--- project/js/pot.js
+++ project/js/pot.js
@@ -49,4 +49,25 @@ $(document).keypress(function(event) {
     } else if (event.charCode == 114) {
         resetTimer();
     }
+    //TODO: add "Enter" event
 });
+// words randomization
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
